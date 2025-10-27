f44a167b119b271e4149d6b470fd5d336d442f6b
Suraj waghmare <surajwaghmare141@gmail.com>
2025-10-27 23:23:46 +0530

    Create Leetcode 72 Edit Distance 1 Recursion solution

---
+++ Day-13-Dynamic-Programming-Type-LCS/Coding-Exercise-26-Edit-Distance/Leetcode 72 Edit Distance 1 Recursion.py
@@ -0,0 +1,39 @@
+"""Given two strings word1 and word2, return the minimum number of operations required to convert word1 to word2.
+
+You have the following three operations permitted on a word:
+
+•Insert a character
+
+•Delete a character
+
+•Replace a character
+
+Example: 
+
+Input: word1 = "hodse", word2 = "dos"
+Output: 3
+Explanation: 
+hodse -> dodse (replace 'h' with 'd')
+dodse -> dose (remove 'd')
+dose -> dos (remove 'e')"""
+def minDistance(word1, word2):
+    n = len(word1)
+    m = len(word2)
+    def number_of_operations(index1,index2):
+        #base case
+        if index1>n-1 and index2>m-1:
+            return 0
+        if index1>n-1:
+            return m-index2
+        if index2>m-1:
+            return n - index1        
+
+        if word1[index1]==word2[index2]:
+            return number_of_operations(index1+1,index2+1)
+        insert = 1 + number_of_operations(index1,index2+1)
+        delete = 1 + number_of_operations(index1+1,index2)
+        replace = 1 + number_of_operations(index1+1,index2+1)
+        return min(insert,delete,replace)    
+    return number_of_operations(0,0)    
+
+print(minDistance("hodse", "dos"))
