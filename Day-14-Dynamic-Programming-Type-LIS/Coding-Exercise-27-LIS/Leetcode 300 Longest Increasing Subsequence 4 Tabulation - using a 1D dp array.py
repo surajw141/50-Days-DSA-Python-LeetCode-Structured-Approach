d584c632604c905ef13fb090e7a027f101a4dd72
Suraj waghmare <surajwaghmare141@gmail.com>
2025-10-28 19:14:58 +0530

    Create Leetcode 300 Longest Increasing Subsequence 4 Tabulation - using a 1D dp array approach

---
+++ Day-14-Dynamic-Programming-Type-LIS/Coding-Exercise-27-LIS/Leetcode 300 Longest Increasing Subsequence 4 Tabulation - using a 1D dp array.py
@@ -0,0 +1,13 @@
+def lengthOfLIS(nums):
+    n = len(nums)
+    dp = [1]*(n)
+    max =1
+
+    for i in range(1,n):
+        for j in range(i):
+            if nums[i]>nums[j] and dp[j]+1>dp[i]:
+                dp[i] = dp[j]+1
+        if dp[i]>max:max = dp[i]
+    return max
+#use it
+print(lengthOfLIS([300,9,2,5,3,7,500,400]))
