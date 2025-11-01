662c64f42ccf9dc5b75099e53d3876e14283e818
Suraj waghmare <surajwaghmare141@gmail.com>
2025-11-01 21:28:42 +0530

    Update Leetcode 152 Maximum Product Subarray sol

--- Day-18-Dynamic-Programming-Type-Kadane's-Algorithm/Coding-Exercise-38-Max-Subarray/Leetcode 152 Maximum Product Subarray.py
+++ Day-18-Dynamic-Programming-Type-Kadane's-Algorithm/Coding-Exercise-38-Max-Subarray/Leetcode 152 Maximum Product Subarray.py
@@ -8,13 +8,20 @@ Input: nums = [3,3,-2,4]
 Output: 9
 Explanation: [3,3] has the largest product 9."""
 
-def maxProductSubarray(nums):
+"""Your code failed this test
+Error details
+2 != 6 : Should be 6"""
+
+def maxProduct(nums):
     #Write code here 
-    max_product = float('-inf')
-    curr_product = 1
-    for num in nums:
-        curr_product = max(num, curr_product * num)
-        max_product = max(max_product, curr_product)
+    max_product = nums[0]
+    curr_max = nums[0]
+    curr_min = nums[0]
+    for i in range(1, len(nums)):
+        temp = curr_max
+        curr_max = max(nums[i], nums[i]*curr_max, nums[i]*curr_min)
+        curr_min = min(nums[i], nums[i]*temp, nums[i]*curr_min)
+        max_product = max(max_product, curr_max)
     return max_product
 
-print(maxProductSubarray([3,3,-2,4]))
+print(maxProduct([3,3,-2,4]))
