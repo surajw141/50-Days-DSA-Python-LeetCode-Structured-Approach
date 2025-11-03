7e533979becc82f86b09cf73b65c8c48cb8618c2
Suraj waghmare <surajwaghmare141@gmail.com>
2025-11-03 11:09:49 +0530

    Create Leetcode 55 Jump Game Greedy Approach sol

---
+++ Day-20-Greedy-Algorithm/Coding-Exercise-41-Jump-Game-1/Leetcode 55 Jump Game Greedy Approach.py
@@ -0,0 +1,14 @@
+"""You are given an integer array nums. You are initially positioned at the array's first index, and each element in the array represents your maximum jump length at that position.
+
+Return true if you can reach the last index, or false otherwise."""
+
+def canJump(nums):
+    n = len(nums)
+    max_index = 0
+    for i in range(n):
+        if i > max_index:return False
+        max_index = max(max_index, i + nums[i])
+        if max_index >= n - 1:
+            return True
+        
+print(canJump([2,3,1,1,4]))  # ✅ should return True
