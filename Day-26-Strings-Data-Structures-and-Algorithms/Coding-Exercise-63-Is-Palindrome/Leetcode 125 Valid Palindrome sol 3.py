506066799892a88fd6c71ccebe51101c253999c3
Suraj waghmare <surajwaghmare141@gmail.com>
2025-11-11 18:58:58 +0530

    Create Leetcode 125 Valid Palindrome sol 3

---
+++ Day-26-Strings-Data-Structures-and-Algorithms/Coding-Exercise-63-Is-Palindrome/Leetcode 125 Valid Palindrome sol 3.py
@@ -0,0 +1,12 @@
+def is_palindrome3(str):
+    i = 0
+    j = len(str) - 1
+    while i < j:
+        if str[i] != str[j]:
+            return False
+        i += 1
+        j -= 1
+        
+    return True
+
+print(is_palindrome3("a"))
